b9de4a75e684c1bdf2919c0bfc69fe987cf2c93c
Giulia Di Canto <g.dicanto@gmail.com>
2024-03-21 19:42:27 +0100

    Form registrati completato con rotte

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,21 +18,33 @@
                         </ul>
                     </div>
                 @endif
-                <form>
+                <form class="card p-5 shadow" action="{{route('register')}}" method="POST">
+                    @csrf
                     <div class="mb-3">
-                      <label for="exampleInputEmail1" class="form-label">Email address</label>
-                      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                      <label for="username" class="form-label">Nome:</label>
+                      <input type="text" name="name" class="form-control" id="username" value={{ old (name)}}>
                     </div>
+
                     <div class="mb-3">
-                      <label for="exampleInputPassword1" class="form-label">Password</label>
-                      <input type="password" class="form-control" id="exampleInputPassword1">
-                    </div>
-                    <div class="mb-3 form-check">
-                      <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                      <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                        <label for="email" class="form-label">Email:</label>
+                        <input type="email" name="email" class="form-control" id="email" value={{ old (email)}}>
+                      </div>
+
+                      <div class="mb-3">
+                        <label for="password" class="form-label">Password:</label>
+                        <input type="password" name="password" class="form-control" id="password">
+                      </div>
+
+                      <div class="mb-3">
+                        <label for="password_confirmation" class="form-label">Password:</label>
+                        <input type="password" name="password_confirmarion" class="form-control" id="password_confirmation">
+                      </div>
+
+                      <div class="mb-3">
+                       <button class="btn bg-info text-white">Registrati</button>
+                       <p class="small-mt-2">Già registrato? <a href="{{ route ('login')}}"></a>Clicca qui</p>
+                      </div>
+                
                   </form>
 
             </div>
