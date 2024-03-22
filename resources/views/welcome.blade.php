e3bc771dfcb59973d6f571a8a0fd5b53c576be1d
Giulia Di Canto <g.dicanto@gmail.com>
2024-03-22 21:41:00 +0100

    database collegato-prova user register superata

--- resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-<x-layout.main>
-    <div class="container-fluid p-5 bg-info text-center text-white">
-        <div class="row justify-content-center">
-            <h2 class="display-1">
-                The Aulab Post
-            </h2>
-        </div>
-    </div>
-</x-layout.main>
--- routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PublicController;
-
-Route::get('/', [PublicController::class , 'homepage'])->name('homepage');
