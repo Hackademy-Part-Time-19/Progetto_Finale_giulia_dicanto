3ad75858dff3c669c2d01e83ed5e9cc5a111d901
Giulia Di Canto <g.dicanto@gmail.com>
2024-03-25 18:52:04 +0100

    relazioni article-user

---
+++ resources/views/components/layout/footer.blade.php
@@ -0,0 +1,74 @@
+<footer style="min-height: 60vh;background-color:#3C3A3B;">
+    <div style="border-bottom: #f4f7f6 solid 2px;">
+
+        
+    </div>
+    </div>
+    </div>
+    <div class="row" style="margin-top: 20px;">
+        <div class="col-12 col-sm-6 col-md-4" style="padding: 5px; margin-left: 15px;">
+            <img style="height: 160px; width: 250px;" src="/img/The-Aulab-Post-logowhite-500x320.png"
+                alt="logo music art">
+            <div style="padding-top: 5%;">
+                <p style="color: #f4f7f6; font-size: 12px;">
+                    Copyright © 2023. All rights reserved. <br> Lagengah.
+                </p>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4" style="align-items: center; justify-content: center;">
+            <p style="color: #f4f7f6; font-weight: 200; text-align: center;">SOCIAL</p>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4" style="align-items:center; justify-content: center;">
+
+            <h6 style="color: #f4f7f6; width: 35%;">SUBSCRIBE</h6>
+            <div class="container-fluid" style="justify-content: left;">
+                <p style="color: #f4f7f6; font-size: 14px; margin-right: 10px; flex: 1;"> Enter your mail to receive
+                    relevant
+                    messaging tips and event</p>
+
+                <div class="search-panels" style="flex: 2;">
+                    <div class="search-group">
+                        <input required="" type="text" name="text" autocomplete="on" class="input">
+                        <label class="enter-label">E-Mail</label>
+                        <div class="btn-box">
+                            <button class="btn-search">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                                    <path
+                                        d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z">
+                                    </path>
+                                    <circle id="svg-circle" cx="208" cy="208" r="144"></circle>
+                                </svg>
+                            </button>
+                        </div>
+                        <div class="btn-box-x">
+                            <button class="btn-cleare">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
+                                    <path
+                                        d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
+                                        id="cleare-line"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <button style="flex:0.3;">
+                    <div class="svg-wrapper-1">
+                        <div class="svg-wrapper">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                <path fill="none" d="M0 0h24v24H0z"></path>
+                                <path fill="currentColor"
+                                    d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z">
+                                </path>
+                            </svg>
+                        </div>
+                    </div>
+                    <span>Subscribe</span>
+                </button>
+
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<x-layout.main>
+    <div class="container-fluid p-5 bg-info text-center text-white">
+        <div class="row justify-content-center">
+            <h2 class="display-1">
+                The Aulab Post
+            </h2>
+        </div>
+    </div>
+</x-layout.main>
+
+<x-layout.footer></x-layout.footer>
